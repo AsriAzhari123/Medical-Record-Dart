e9ff48ef555efdaf30ef75c709eb66a4963f5f06
Asri Azhari <119406840+AsriAzhari123@users.noreply.github.com>
2023-07-15 21:02:23 +0700

    tamabahan widget loading

--- lib/Layer/Login.dart
+++ lib/Layer/Login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 import '../Provider/user_data.dart';
@@ -20,216 +20,250 @@ class _MyLoginState extends State<MyLogin> {
   TextEditingController passwordController = TextEditingController();
   bool isNSIPEmpty = false;
   bool isKataSandiEmpty = false;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     var prov = Provider.of<DataUser>(context);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-            child: Column(
-              children: [
-                Container(
-                  height: 220,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(127, 218, 244, 100),
-                    borderRadius: BorderRadius.only(
-                      bottomRight: Radius.elliptical(80, 80),
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Image.asset(
-                          'images/profile.png',
-                          width: 140,
-                          height: 110,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Tambahkan crossAxisAlignment
+                  children: [
+                    Container(
+                      height: 220,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(127, 218, 244, 100),
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.elliptical(80, 80),
                         ),
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          "MASUK",
-                          style: TextStyle(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 8.0),
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          padding: EdgeInsets.symmetric(horizontal: 25),
-                          child: TextFormField(
-                            controller: nsipController,
-                            decoration: InputDecoration(
-                              labelText: "NSIP",
-                              prefixIcon: Icon(Icons.person),
-                              counterText: "",
-                              errorText: isNSIPEmpty ? 'Harap isi NSIP' : null,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'images/profile.png',
+                              width: 140,
+                              height: 110,
                             ),
-                            maxLength: 9,
-                            inputFormatters: <TextInputFormatter>[
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                            keyboardType: TextInputType.number,
-                            onChanged: (value) {
-                              setState(() {
-                                isNSIPEmpty = value.isEmpty;
-                              });
-                            },
                           ),
-                        ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              "MASUK",
+                              style: TextStyle(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.symmetric(horizontal: 25),
-                    child: TextFormField(
-                      controller: passwordController,
-                      obscureText: check,
-                      decoration: InputDecoration(
-                        labelText: "Kata Sandi",
-                        prefixIcon: Icon(Icons.lock),
-                        suffixIcon: IconButton(
-                          onPressed: () {
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              padding: EdgeInsets.symmetric(horizontal: 25),
+                              child: TextFormField(
+                                controller: nsipController,
+                                decoration: InputDecoration(
+                                  labelText: "NSIP",
+                                  prefixIcon: Icon(Icons.person),
+                                  counterText: "",
+                                  errorText:
+                                      isNSIPEmpty ? 'Harap isi NSIP' : null,
+                                ),
+                                maxLength: 9,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                                keyboardType: TextInputType.number,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isNSIPEmpty = value.isEmpty;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: TextFormField(
+                          controller: passwordController,
+                          obscureText: check,
+                          decoration: InputDecoration(
+                            labelText: "Kata Sandi",
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  check = !check;
+                                });
+                              },
+                              icon: Icon(
+                                check ? Icons.visibility_off : Icons.visibility,
+                              ),
+                            ),
+                            errorText: isKataSandiEmpty
+                                ? 'Harap isi Kata Sandi'
+                                : null,
+                          ),
+                          onChanged: (value) {
                             setState(() {
-                              check = !check;
+                              isKataSandiEmpty = value.isEmpty;
                             });
                           },
-                          icon: Icon(
-                            check ? Icons.visibility_off : Icons.visibility,
-                          ),
                         ),
-                        errorText:
-                            isKataSandiEmpty ? 'Harap isi Kata Sandi' : null,
                       ),
-                      onChanged: (value) {
-                        setState(() {
-                          isKataSandiEmpty = value.isEmpty;
-                        });
-                      },
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(22.0),
-                  child: SizedBox(
-                    height: 47,
-                    width: 193,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          isNSIPEmpty = nsipController.text.isEmpty;
+                    Padding(
+                      padding: EdgeInsets.all(22.0),
+                      child: SizedBox(
+                        height: 47,
+                        width: 193,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              // Set loading to true before initiating the login process
+                              isLoading = true;
+                              isNSIPEmpty = nsipController.text.isEmpty;
+                              isKataSandiEmpty =
+                                  passwordController.text.isEmpty;
+                            });
 
-                          isKataSandiEmpty = passwordController.text.isEmpty;
-                        });
-                        var data = (prov.datauser['user']
-                                as List<Map<String, String>>)
-                            .any((user) => nsipController.text != user['NSIP']);
-                        if (nsipController.text.isNotEmpty &&
-                            passwordController.text.isNotEmpty &&
-                            data) {
-                          showDialog(
-                            context: context,
-                            builder: (context) => AlertDialog(
-                              title: Text('Akun Tidak Valid'),
-                              content: Text(
-                                'Akun yang anda masukkan tidak terdaftar',
-                              ),
-                              actions: <Widget>[
-                                ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('OK'),
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                        if (prov.login(
-                          nsipController.text,
-                          passwordController.text,
-                        )) {
-                          Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(builder: (context) => MyHome()),
-                              (route) => false);
-                        }
-                      },
-                      child: Text(
-                        "Masuk",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(0, 154, 205, 100),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 16,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Belum Punya Akun ?",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => MyRegister(),
-                              ),
-                            );
+                            // Simulate a delay to show the loading indicator
+                            Future.delayed(Duration(seconds: 2), () {
+                              var data = (prov.datauser['user']
+                                      as List<Map<String, String>>)
+                                  .any((user) =>
+                                      nsipController.text != user['NSIP']);
+                              if (nsipController.text.isNotEmpty &&
+                                  passwordController.text.isNotEmpty &&
+                                  data) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Akun Tidak Valid'),
+                                    content: Text(
+                                        'Akun yang anda masukkan tidak terdaftar'),
+                                    actions: <Widget>[
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                              if (prov.login(nsipController.text,
+                                  passwordController.text)) {
+                                // Navigate to the home page after successful login
+                                Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MyHome()),
+                                  (route) => false,
+                                );
+                              }
+
+                              // Set loading to false after the login process is complete
+                              setState(() {
+                                isLoading = false;
+                              });
+                            });
                           },
                           child: Text(
-                            "Daftar",
+                            "Masuk",
                             style: TextStyle(
-                              color: Color.fromRGBO(0, 154, 205, 100),
+                              color: Colors.black,
+                              fontSize: 18,
                               fontWeight: FontWeight.bold,
-                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color.fromRGBO(0, 154, 205, 100),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        height: 16,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Belum Punya Akun ?",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => MyRegister(),
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "Daftar",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(0, 154, 205, 100),
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          Positioned.fill(
+            top: 200, // Sesuaikan dengan posisi yang diinginkan
+            child: Align(
+              alignment: Alignment.center,
+              child: Visibility(
+                visible: isLoading,
+                child: SpinKitCircle(
+                  color: Colors.blue,
+                  size: 50.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
